ed71a29b020e1be1929de31a1ca9dcc92aa31f1d
Pia <piamunozt@gmail.com>
2024-11-27 13:15:34 +0100

    Added menu options from array of constants

---
+++ EscapeRoom/src/main/java/utils/MenuOptions.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class MenuOptions {
+    static final String m1 = "Create new Escape Room. ";
+    static final String m2 = "Add new Room to the Escape Room. ";
+    static final String m3 = "Add new Enigma to a Room.";
+    static final String m4 = "Add new Clue for an Enigma.";
+    static final String m5 = "Add new Decoration object to a/some Room(s).";
+    static final String m6 = "Show inventory (Rooms, Enigmas, Clues and Decorations). ";
+    static final String m7 = "Show total inventory value.";
+    static final String m8 = "Remove Room, Enigma, Clue or Decoration. ";
+    static final String m9 = "Add new user. ";
+    static final String m10 = "Create new game and generate ticket. ";
+    static final String m11 = "Show total sales. ";
+    static public final String close = "Close.";
+
+    static public String[] options = {m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11};
+}
