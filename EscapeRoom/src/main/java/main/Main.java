4b34d58fd504a8dc7467bfbc0e2e7e5ae2b4ee36
Toni Sau <tonisau@users.noreply.github.com>
2024-11-27 13:59:15 +0100

    UPDATE Main with DbConnection

--- EscapeRoom/src/main/java/main/Main.java
+++ EscapeRoom/src/main/java/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
+import connections.DbConnection;
 import managers.EscapeRoomManager;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+        DbConnection dbconnection = new DbConnection();
+        Connection connection = dbconnection.getConnection();
+        if (connection !=null){
+            System.out.println("Connection successful");
+        }
 
         EscapeRoomManager escapeRoomManager = new EscapeRoomManager();
         escapeRoomManager.start();
