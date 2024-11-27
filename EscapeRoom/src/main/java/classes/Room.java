1c7b5ed2a93fed1d083084fa872e10bac6799f05
Toni Sau <tonisau@users.noreply.github.com>
2024-11-27 12:57:06 +0100

    add Room

--- EscapeRoom/src/main/java/classes/Room.java
+++ EscapeRoom/src/main/java/classes/Room.java
@@ -1,4 +1,56 @@
 package classes;
 
+import classes.enums.Level;
+
 public class Room {
+    private int idRoom;
+    private String name;
+    private double price;
+    private Level level;
+
+    public Room(){
+
+    }
+
+    public int getIdRoom() {
+        return idRoom;
+    }
+
+    public void setIdRoom(int idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "idRoom=" + idRoom +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", level=" + level +
+                '}';
+    }
 }
---
+++ EscapeRoom/src/main/java/classes/enums/Level.java
@@ -0,0 +1,35 @@
+package classes.enums;
+
+import static java.time.chrono.JapaneseEra.values;
+
+public enum Level {
+
+    LOW(1, "Low"),
+    MEDIUM(2, "Mediu"),
+    HIGH(3, "High");
+
+    private final int VALUE;
+    private String levelName;
+
+    private Level(int VALUE, String levelName) {
+        this.VALUE = VALUE;
+        this.levelName = levelName;
+    }
+
+    public int getVALUE() {
+        return VALUE;
+    }
+
+    public String getLevelName() {
+        return levelName;
+    }
+
+    public static Level findByValue(int value) {
+        for (Level level : values()) {
+            if (level.getVALUE() == value) {
+                return level;
+            }
+        }
+        return null;
+    }
+}
