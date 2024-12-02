ba2775b1256c365c3b30d94acd53ca0b3dcf0ae9
g-lemoing <le.moing.gwenael@gmail.com>
2024-12-02 18:51:34 +0100

    GameManager > initial commit

---
+++ EscapeRoom/src/main/java/managers/GameManager.java
@@ -0,0 +1,62 @@
+package managers;
+
+import classes.Room;
+import classes.User;
+import classes.item.implementations.Enigma;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class GameManager {
+    private List<User> players;
+    private Room room;
+    private ItemDAOImpl itemDAO;
+    HashMap<User, Enigma> certificates;
+    HashMap<User, Gift> gifts;
+
+
+    public GameManager(List<User> players, Room room, ItemDAOImpl itemDAO){
+        this.players = players;
+        this.room = room;
+        this.itemDAO = itemDAO;
+    }
+
+    public void playGame(){
+        this.certificates = resolveEnigmas();
+        this.gifts = grantGifts();
+    }
+    public HashMap<User, Enigma> resolveEnigmas(){
+        // Replace following code with Get Enigma list of the room
+        List<Enigma> enigmas = new ArrayList<>();
+        enigmas.add(new Enigma("Enigma1", 12.00));
+        enigmas.add(new Enigma("Enigma2", 14.50));
+
+        HashMap<User, Enigma> certificates = new HashMap<>();
+        enigmas.forEach(enigma -> {
+            for (User player : players){
+                if (solveEnigma()){
+                    certificates.put(player, enigma);
+                    break;
+                }
+            }
+        });
+        return certificates;
+    }
+
+    public boolean solveEnigma(){
+        return new Random().nextBoolean();
+    }
+
+    public HashMap<User, Gift> grantGifts(){
+        HashMap<User, Gift> grantedGifts = new HashMap<User, Gift>();
+        List<Gift> availableGifts = new ArrayList<>();
+        this.certificates.forEach(((user, enigma) -> {
+            Gift gift = availableGifts.get(new Random().nextInt(availableGifts.size()));
+            grantedGifts.put(user, gift);
+        }));
+        // write grantedGifts in BDD
+        return grantedGifts;
+    }
+}
