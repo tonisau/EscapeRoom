04388ee083db753ee9e7fd65b40e11e3025a9136
g-lemoing <le.moing.gwenael@gmail.com>
2024-12-03 13:55:25 +0100

    Parser > add onCallbackBoolean() method in interface and classes which implement it

--- EscapeRoom/src/main/java/DAO/implementations/UserDAOImpl.java
+++ EscapeRoom/src/main/java/DAO/implementations/UserDAOImpl.java
@@ -5,12 +5,15 @@ import DAO.interfaces.UserDAO;
 import classes.User;
 import classes.enums.Material;
 import classes.item.Item;
+import classes.item.implementations.Enigma;
 import connections.DbConnectionImpl;
 import connections.attribute.Attribute;
 import connections.attribute.Query;
 import connections.callback.ParsingCallback;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO, ParsingCallback<User> {
@@ -63,20 +66,38 @@ public class UserDAOImpl implements UserDAO, ParsingCallback<User> {
 
     @Override
     public List<User> showData() {
-        return List.of();
+        List<User> users = new ArrayList<>();
+
+        List<Attribute> queryAttributeList = List.of();
+        List<Attribute> outputAttributes = Arrays.asList(
+                new Attribute<>(IDUSER, null, Integer.class),
+                new Attribute<>(NAME, null, String.class),
+                new Attribute<>(EMAIL, null, String.class),
+                new Attribute<>(IS_SUBSCRIBER, null, boolean.class));
+
+        List<HashSet<Attribute>> userList = dbConnection.query(Query.SHOWUSERS, queryAttributeList, outputAttributes);
+
+        if (userList.isEmpty()) return List.of();
+
+        for (HashSet<Attribute> attributeValues: userList) {
+            User user = new User();
+            parser.parseObject(user, attributeValues);
+            users.add(user);
+        }
+        return users;
     }
 
     @Override
     public void onCallbackString(User object, Attribute<String> attribute) {
         switch (attribute.getName()){
-            case
+            case NAME -> object.setName(attribute.getValue());
+            case EMAIL -> object.setEmail(attribute.getValue());
         }
-
     }
 
     @Override
     public void onCallbackInt(User object, Attribute<Integer> attribute) {
-
+        if (attribute.getName().equals(IDUSER)) object.setId(attribute.getValue());
     }
 
     @Override
@@ -87,5 +108,10 @@ public class UserDAOImpl implements UserDAO, ParsingCallback<User> {
     @Override
     public void onCallbackMaterial(User object, Attribute<Material> attribute) {
 
+    }
+
+    @Override
+    public void onCallbackBoolean(User object, Attribute<Boolean> attribute){
+        if (attribute.getName().equals(IS_SUBSCRIBER)) object.setIsSuscriber(attribute.getValue());
     }
 }
