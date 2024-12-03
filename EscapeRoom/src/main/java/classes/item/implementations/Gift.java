a8d3f80b2421898f6368e704f2ee34b96d41a819
Pia <piamunozt@gmail.com>
2024-12-03 20:30:01 +0100

    remove public

--- EscapeRoom/src/main/java/classes/item/implementations/Gift.java
+++ EscapeRoom/src/main/java/classes/item/implementations/Gift.java
@@ -3,7 +3,7 @@ package classes.item.implementations;
 import classes.item.Item;
 
 public class Gift extends Item {
-    public Gift(String name, Double price) {
+    Gift(String name, Double price) {
         super(name, price);
     }
 
