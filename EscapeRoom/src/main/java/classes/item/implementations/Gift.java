dcb69b60ab4f6b6e1ca830d469c09c5505c93be9
g-lemoing <le.moing.gwenael@gmail.com>
2024-12-03 18:16:18 +0100

    Gift > Add builder Gift(){}

--- EscapeRoom/src/main/java/classes/item/implementations/Gift.java
+++ EscapeRoom/src/main/java/classes/item/implementations/Gift.java
@@ -3,6 +3,7 @@ package classes.item.implementations;
 import classes.item.Item;
 
 public class Gift extends Item {
+    public Gift(){};
     public Gift(String name, double price) {
         super(name, price);
     }
