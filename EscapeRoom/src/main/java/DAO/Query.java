ef09c48e3cabf1d95ab7f23205e1e9433ceee7eb
g-lemoing <le.moing.gwenael@gmail.com>
2024-12-06 16:28:09 +0100

    Qurey> GETGIFTBYID

--- EscapeRoom/src/main/java/DAO/Query.java
+++ EscapeRoom/src/main/java/DAO/Query.java
@@ -28,7 +28,7 @@ public class Query {
     public static final String GETGIFTBYUSER = "select idgift, name from user_has_gift uhg join gift g ON uhg.gift_idgift = g.idgift where uhg.user_iduser = ?";
     public static final String GETALLGIFTS = "SELECT idgift, name FROM gift";
     public static final String DELETEGIFT = "DELETE FROM gift WHERE idgift = ?";
-    public static final String GETGIFTBYID = "SELECT idgift, name, 0 AS price FROM gift WHERE idgift = ?";
+    public static final String GETGIFTBYID = "SELECT idgift, name FROM gift WHERE idgift = ?";
 
     public static final String CREATEROOM = "INSERT INTO room (name, price, level, escaperoom_idescaperoom) VALUES (?, ?, ?, ?)";
     public static final String GETROOMBYESCAPEROOM = "SELECT idroom, name, price, level FROM room WHERE escaperoom_idescaperoom = ?;";
