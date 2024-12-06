00c8d5ff1d3e4dd60dc57865ede3370a4b8573da
g-lemoing <le.moing.gwenael@gmail.com>
2024-12-06 17:47:57 +0100

    User> add isSubscriber parameter to builder

--- EscapeRoom/src/main/java/classes/User.java
+++ EscapeRoom/src/main/java/classes/User.java
@@ -10,9 +10,10 @@ public class User implements Subscriber {
 
     public User(){};
 
-    public User(String name, String email) {
+    public User(String name, String email, Boolean isSuscriber) {
         this.name = name;
         this.email = email;
+        this.isSuscriber = isSuscriber;
     }
 
     public User(Integer id, String name, String email, Boolean isSuscriber) {
